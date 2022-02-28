f806648d73d59b28f6cbec3a3cc5375f180044ed
PatelHinal53 <86098100+PatelHinal53@users.noreply.github.com>
2022-02-28 16:08:37 +0530

    Update ForgotpassA.cs

--- Login_Register/ForgotpassA.cs
+++ Login_Register/ForgotpassA.cs
@@ -1,7 +1,9 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
+using Android.Text;
 using Android.Views;
 using Android.Widget;
 using System;
@@ -15,14 +17,29 @@ namespace Login_Register
     public class ForgotpassA : Activity
     {
         Button button1;
+        TextView txtforgot;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.forgot);
             // Create your application here
 
+            txtforgot = FindViewById<TextView>(Resource.Id.txtforgot);
             button1 = FindViewById<Button>(Resource.Id.resetbtn);
             button1.Click += Reset_Click;
+
+            TextPaint paint = txtforgot.Paint;
+            float width = paint.MeasureText(txtforgot.Text);
+            int[] vs = new int[]
+            {
+                Color.ParseColor("#4A148C"),
+                Color.ParseColor("#4A148C"),
+                Color.ParseColor("#00008B"),
+                Color.ParseColor("#209FF1"),
+                Color.ParseColor("#209FF1"),
+            };
+            Shader textshade = new LinearGradient(0, 0, width, txtforgot.TextSize, vs, null, Shader.TileMode.Clamp);
+            txtforgot.Paint.SetShader(textshade);
         }
 
         private void Reset_Click(object sender, EventArgs e)
