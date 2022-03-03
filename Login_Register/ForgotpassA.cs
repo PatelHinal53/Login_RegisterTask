bd86851eb32baf58eb77707ac4adec728477bb78
PatelHinal53 <86098100+PatelHinal53@users.noreply.github.com>
2022-03-03 13:14:08 +0530

    Update ForgotpassA.cs

--- Login_Register/ForgotpassA.cs
+++ Login_Register/ForgotpassA.cs
@@ -1,4 +1,4 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content;
 using Android.Graphics;
 using Android.OS;
@@ -13,40 +13,64 @@ using System.Text;
 
 namespace Login_Register
 {
-    [Activity(Label = "ForgotpassA")]
+    [Activity(Label = "ForgotpassA" , Theme = "@style/AppTheme")]
     public class ForgotpassA : Activity
     {
-        Button button1;
-        TextView txtforgot;
+        Button buttonReset;
+        TextView textViewForgot;
+        EditText editTextNewPass, editTextConfirmPass;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.forgot);
             // Create your application here
 
-            txtforgot = FindViewById<TextView>(Resource.Id.txtforgot);
-            button1 = FindViewById<Button>(Resource.Id.resetbtn);
-            button1.Click += Reset_Click;
+            textViewForgot = FindViewById<TextView>(Resource.Id.textViewForgot);
+            editTextNewPass = FindViewById<EditText>(Resource.Id.editTextNewPass);
+            editTextConfirmPass = FindViewById<EditText>(Resource.Id.editTextConfirmPass);
+            buttonReset = FindViewById<Button>(Resource.Id.buttonReset);
 
-            TextPaint paint = txtforgot.Paint;
-            float width = paint.MeasureText(txtforgot.Text);
+            buttonReset.Click += Reset_Click;
+            MultiColor();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            if (editTextNewPass.Text == "")
+            {
+                editTextNewPass.Error = "Enter NewPass Password ";
+            }
+            else if (editTextConfirmPass.Text == "")
+            {
+                editTextConfirmPass.Error = "Enter ConfirmPass Password";
+            }
+            else if (editTextNewPass.Text != editTextConfirmPass.Text)
+            {
+                Toast.MakeText(this, "Your NewPass Or ConfirmPass Not Same.", ToastLength.Short).Show();
+            }
+            else
+            {
+                Intent b = new Intent(this, typeof(MainActivity));
+                StartActivity(b);
+                Toast.MakeText(this, "Reset your Password Successfully", ToastLength.Short).Show();
+            }
+            
+        }
+        private void MultiColor()
+        {
+            TextPaint paint = textViewForgot.Paint;
+            float width = paint.MeasureText(textViewForgot.Text);
             int[] vs = new int[]
             {
                 Color.ParseColor("#4A148C"),
                 Color.ParseColor("#4A148C"),
                 Color.ParseColor("#00008B"),
-                Color.ParseColor("#209FF1"),
-                Color.ParseColor("#209FF1"),
+                Color.ParseColor("#3c70d9"),
+                Color.ParseColor("#3c70d9"),
             };
-            Shader textshade = new LinearGradient(0, 0, width, txtforgot.TextSize, vs, null, Shader.TileMode.Clamp);
-            txtforgot.Paint.SetShader(textshade);
+            Shader textshade = new LinearGradient(0, 0, width, textViewForgot.TextSize, vs, null, Shader.TileMode.Clamp);
+            textViewForgot.Paint.SetShader(textshade);
         }
-
-        private void Reset_Click(object sender, EventArgs e)
-        {
-            Intent b = new Intent(this, typeof(MainActivity));
-            StartActivity(b);
-            Toast.MakeText(this, "Reset your Password Successfully" ,ToastLength.Short).Show();
-        }
+        
     }
 }
